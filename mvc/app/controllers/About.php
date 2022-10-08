746c9114ec464bbdc212417dd5ef2058a532ad68
agungSeptiana <203040102@mail.unpas.ac.id>
2022-10-08 07:17:50 +0700

    Menambahkan controller mvc

---
+++ mvc/app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php 
+
+class About {
+    public function index($nama = "Agung", $pekerjaan = "Backend developer") {
+        echo "Halo nama saya $nama, saya adalah seorang $pekerjaan";
+    } 
+    public function page() {
+        echo 'About/page';
+    }
+}
--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -1,9 +1,38 @@
 <?php 
 
 class App {
+    protected $controler = 'Home';
+    protected $method = 'index';
+    protected $param = [];
+
     public function __construct() {
         $url = $this->parseUrl();
-        var_dump($url);
+        
+        //Controller
+        if( file_exists('../app/controllers/' . $url[0] . '.php') ) {
+            $this->controler = $url[0];
+            unset($url[0]);
+        }
+        
+        require_once '../app/controllers/' . $this->controler . '.php';
+        $this->controler = new $this->controler;
+
+        //Method
+        if(isset($url[1])) {
+            if(method_exists($this->controler, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        //Parameter
+        if(!empty($url)) {
+            $this->param = array_values($url);
+        }
+
+        //jalankan controler & method, serta kirimkan params jika ada
+        call_user_func_array([$this->controler, $this->method], $this->param);
+
     }
 
     public function parseUrl() {
