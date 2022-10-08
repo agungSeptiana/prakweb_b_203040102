60fb260111be1d4ef5e8a4228ea7315102fc296c
agungSeptiana <203040102@mail.unpas.ac.id>
2022-10-08 14:19:24 +0700

    Menambahkan Database Wrapper MVC

--- mvc/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php 
-
-define('BASEURL', 'http://localhost/prakweb_b_203040102/mvc/public');
--- mvc/app/models/Mahasiswa_model.php
+++ mvc/app/models/Mahasiswa_model.php
@@ -1,23 +1,21 @@
 <?php 
 
 class Mahasiswa_model {
-    private $dbh;   //database handler
-    private $stat;
+    private $table = 'mahasiswa';
+    private $db;
 
-    public function __construct() {
-        //data source name
-        $dsn = 'mysql:host=localhost;dbname=prakweb_b_203040102';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch(PDOException $e) {
-            die($e->getMessage());
-        }
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+    public function getAllMahasiswa() {
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
     }
 
-    public function getAllMahasiswa() {
-        $this->stat = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stat->execute();
-        return $this->stat->fetchAll(PDO::FETCH_ASSOC);
+    public function getMahasiswaById($id) {
+        $this->db->query( 'SELECT * FROM ' . $this->table . ' WHERE id=:id' );
+        $this->db->bind('id', $id);
+        return $this->db->single();
     }
 }
