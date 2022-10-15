6593fbbe75d3554f970dcb3a23f1a6fc30df0664
agungSeptiana <203040102@mail.unpas.ac.id>
2022-10-15 10:50:37 +0700

    Menambahkan Blade Templating Engine

--- Laravel/application/coba-laravel/resources/views/post.blade.php
+++ Laravel/application/coba-laravel/resources/views/post.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AGS Blog | Post</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <h1>Halaman Post</h1>
-</body>
-</html>
+@extends('layouts.main')
+    @section('container')
+        @foreach ($Posts as $post)
+            <article class="mb-5">
+                <h2>
+                    <a href="/post/{{ $post["slug"] }}">{{ $post["title"] }}</a>
+                </h2>
+                <h5>By :{{ $post["author"] }}</h5>
+                <p>{{ $post["body"] }}</p>
+            </article>
+        @endforeach
+    @endsection
+
+
+        
