b4b35c5a8b108c5c5b393203e8eca56677150671
agungSeptiana <203040102@mail.unpas.ac.id>
2022-10-15 07:00:35 +0700

    Menambahkan Struktur folder routes dan view Laravel

--- Laravel/application/coba-laravel/routes/web.php
+++ Laravel/application/coba-laravel/routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Agung Septiana",
+        "email" => "203040102@mail.unpas.ac.id",
+        "image" => "agung1.jpg"
+    ]);
+});
+Route::get('/post', function () {
+    return view('post');
+});
+
+
